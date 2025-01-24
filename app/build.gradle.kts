020282e7a7ea9e45ade2722a1d5a42e2100c2f5e
Joshua <joshuaarbis708@gmail.com>
2025-01-24 17:22:26 +0700

    navigation custom mapbox

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.mapboxnavigationapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.mapboxnavigationapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation("com.mapbox.maps:android:11.9.1")
+    implementation("com.mapbox.navigationcore:android:3.6.0")
+    implementation("com.mapbox.navigationcore:navigation:3.6.0")
+    implementation("com.mapbox.navigationcore:copilot:3.6.0")
+    implementation("com.mapbox.navigationcore:ui-maps:3.6.0")
+    implementation("com.mapbox.navigationcore:voice:3.6.0")
+    implementation("com.mapbox.navigationcore:tripdata:3.6.0")
+    implementation("com.mapbox.navigationcore:ui-components:3.6.0")
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.appcompat)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
